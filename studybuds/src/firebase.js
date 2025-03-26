55b6c1f4407990c7dde66dba979eed86fa7bd7ea
theUno23 <JanDAlvarez1123@hotmail.com>
2025-03-25 20:34:38 -0400

    Firebase + React Files

---
+++ studybuds/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8y1-g0sSDxU4zX0bv7Zi17EYuo210Aq8",
+  authDomain: "studybuds-cb0e4.firebaseapp.com",
+  projectId: "studybuds-cb0e4",
+  storageBucket: "studybuds-cb0e4.firebasestorage.app",
+  messagingSenderId: "339137507957",
+  appId: "1:339137507957:web:48124044bc35d148b57e3c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
