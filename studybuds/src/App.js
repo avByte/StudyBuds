3eca350d553c1b43f1d07d8598ef89b2b1dee66d
theUno23 <JanDAlvarez1123@hotmail.com>
2025-03-31 18:57:28 -0400

    Update Firebase configuration and fix routing issues

--- studybuds/src/App.js
+++ studybuds/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import CreateAccount from "./CreateAccount";
 import Login from "./Login";
 import Questionnaire from "./Questionnaire";
